8533e353e870b157440bc468b2d05555da69448a
KevinJSL <kjl9300@g.rit.edu>
2024-03-21 14:42:19 -0400

    Added back the functions I deleted

--- application.py
+++ application.py
@@ -46,3 +46,23 @@ def search_users(curs:cursor, email:str):
 
 def unfollow(curs:cursor, user_email:str):
     pass
+
+def createAccount(curs):
+    # record time and date of account creation
+    print ("Function to create a account")
+
+def login(curs):
+    print ("Function to login")
+
+def listCollections(curs):
+    print ("Function to print a users collection")
+
+def searchMovie(curs):
+    # Need the user to select by what parameter they are going to be searching by
+    # Take in user input for that parameter and then write queries and return as such
+    print ("Function to search for movies")
+
+
+def getFriends(curs):
+    # Write the querty to get and print friends
+    print ("Function to get friends")
